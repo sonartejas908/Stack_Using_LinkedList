59c17a642d72c00cc4ebc27e0a4d1a97e8e3d343
Tejassonar <sonartejas908@gmail.com>
2021-10-02 21:08:06 +0530

    Contains Node class

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day14Stack_queue_problems
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
